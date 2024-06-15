522ce12cf6f165029d59d2d830dce9cfe04de12e
mallicksiddartha <mallicksiddartha@gmail.com>
2024-06-15 21:53:27 +0200

    Weather app project creation and search bar addition

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'ba8264f6b9msh5aa83eddd7b4a2bp132b8cjsn78ae58c773fe',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
