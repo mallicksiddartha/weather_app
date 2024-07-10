88f68f9fe463233184166b8087dbc1f60043a329
mallicksiddartha <mallicksiddartha@gmail.com>
2024-07-10 12:29:12 +0200

    Added dotenv file and migrated api keys to env file.

--- src/api.js
+++ src/api.js
@@ -2,10 +2,9 @@ export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': 'ba8264f6b9msh5aa83eddd7b4a2bp132b8cjsn78ae58c773fe',
+		'x-rapidapi-key': process.env.REACT_APP_GEO_RAPID_API_KEY,
 		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "6862d13e6831c10fe50a61b5d22e9b7a"
